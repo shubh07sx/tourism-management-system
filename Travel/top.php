8fa5cb299b7f3022752f6a90bc355324f543c2b3
shubh saxena <36452559+shubh07sx@users.noreply.github.com>
2019-12-09 02:37:44 +0530

    Add files via upload

---
+++ Travel/top.php
@@ -0,0 +1,25 @@
+	 <div class="header-top">
+		 <!--container-->
+		  <div class="container">
+			 <div class="top-nav">
+						<div class="logo">
+						<a href="#"><img src="images/logo4.png" id="section-1" class="img-responsive" alt=""/></a>
+						</div>
+						<div class="menu">
+						<ul id="nav">
+							 <li><a href="index.php#section-1" style="color:grey;" onclick="javascript:window.open('index.php#section-1','_self')">                            Home</a></li>
+							 <li><a href="index.php#section-2"  style="color:grey;" onclick="javascript:window.open('index.php#section-2','_self')">                           About</a></li>
+							 <li><a href="index.php#section-3"  style="color:grey;" onclick="javascript:window.open('index.php#section-3','_self')">                             Gallery</a></li>
+                        <li><a href="category.php" style="color:grey;" onclick="javascript:window.open('category.php','_self')">Category</a></li>                             
+							 <li><a href="index.php#section-4"  style="color:grey;" onclick="javascript:window.open('index.php#section-4','_self')">                            Advertisements</a></li>
+							 <li><a href="index.php#section-5" style="color:grey;"   onclick="javascript:window.open('index.php#section-5','_self')">                              Contact</a></li>
+							 <div class="clearfix"></div>
+						 </ul>
+						 </div>
+			 </div>
+			  <div class="clearfix"> </div>
+			
+
+		 </div>
+		 <!--/container-->
+	 </div>
---
+++ Travel/transaction.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>My-tour bootstrap Design website | Home :: w3layouts</title>
+<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+<link href="stylecss.css" rel='stylesheet' type='text/css'/>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css'/>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--js--> 
+<script src="js/jquery.min.js"></script>
+
+<!--/js-->
+<!--animated-css-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+<script>
+ new WOW().init();
+</script>
+</head>
+
+<body>
+<?php include('function.php'); ?>
+<?php include('top.php'); ?>
+
+<h2>Radio Buttons</h2>
+
+<form>
+  <input type="radio" name="gender" value="male" checked> Male<br>
+  <input type="radio" name="gender" value="female"> Female<br>
+  <input type="radio" name="gender" value="other"> Other  
+</form> 
+<?php include('bottom.php'); ?>
+</body>
+</html>
